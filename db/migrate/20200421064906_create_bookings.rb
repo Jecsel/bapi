50c2d2088cbe2476fb7f8768559bda894d9effa0
Ramel <ramel.cabug@biomarking.com>
2020-04-21 14:58:52 +0800

    fixtures

--- db/migrate/20200421064906_create_bookings.rb
+++ db/migrate/20200421064906_create_bookings.rb
@@ -1,7 +1,12 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
-
+      t.belongs_to :patient, index:true
+      t.belongs_to :location, index:true
+      t.belongs_to :slot, index:true
+      t.belongs_to :schedule, index:true
+      t.belongs_to :clinic
+      t.string :amount
       t.timestamps
     end
   end
