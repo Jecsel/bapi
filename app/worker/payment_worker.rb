79a90f8a739006dceaf407e8a4bddaba84c42a9f
Ramel <ramel.cabug@biomarking.com>
2020-04-27 15:35:06 +0800

    fix

--- app/worker/payment_worker.rb
+++ app/worker/payment_worker.rb
@@ -28,7 +28,7 @@ class PaymentWorker
     end
 
     def generate_system_signature
-        __code = "#{ENV["MERCHANT_KEY"]}#{ENV["MERCHANT_CODE"]},#{payment_id}#{payment.ref_no}#{payment.amount.gsub('.', "")}#{payment.currency}1"
+        __code = "#{ENV["MERCHANT_KEY"]}#{ENV["MERCHANT_CODE"]}#{payment_id}#{payment.ref_no}#{payment.amount.gsub('.', "")}#{payment.currency}1"
         p "------"
         p __code
         p "------"
