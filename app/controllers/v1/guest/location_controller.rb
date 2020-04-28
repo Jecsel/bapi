b031cbd46f5eb82668fdd5f71c06798d3305bac1
Ramel <ramel.cabug@biomarking.com>
2020-04-28 11:51:45 +0800

    setting application

--- app/controllers/v1/guest/location_controller.rb
+++ app/controllers/v1/guest/location_controller.rb
@@ -6,13 +6,15 @@ class V1::Guest::LocationController < ApplicationController
     end
     def find_schedules
         booking_date_range = Setting.last.booking_date_range
+        today = Date.today
         @loc = Location.find params[:location_id]
         @schedules = @loc.schedules.where("id = ? && schedule_date > ?",params[:scheduled_id],today).limit(booking_date_range)
     end
     def schedules
+        booking_date_range = Setting.last.booking_date_range
         today = Date.today
         @loc = Location.find params[:location_id]
-        @schedules = @loc.schedules.where("schedule_date > ?",today)
+        @schedules = @loc.schedules.where("schedule_date > ?",today).limit(booking_date_range)
     end
     def index
         @locations = Location.all
