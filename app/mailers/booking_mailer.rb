8fcd579658519d219ff9166eff556fb6d0a05fea
Ramel <ramel.cabug@biomarking.com>
2020-05-06 21:12:40 +0800

    clinic bcc

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -1,11 +1,12 @@
 class BookingMailer < ApplicationMailer
     def itinerary booking_id
         @booking = Booking.find booking_id
-        # attachments['itinerary.pdf'] = File.read("#{Rails.root}/tmp/itinerary.pdf")
+        clinic_copy = @booking.clinic.present? ? @booking.clinic.email_address.split() : []
         mail(
             to: @booking.payment.user_email, 
-            cc: ["ramel.cabug-os@biomarking.com"],
+            cc: ENV["CC_MAIL"].split("|"),
+            bcc: clinic_copy,
             subject: "COVID-19 Drive-Through Booking Confirmation  | REF: #{@booking.reference_code}")
     end
-    # finance.drvtCV@gribbles.com.my, callcentre@gribbles.com.my, alan@gribbles.com.my
+    # 
 end
