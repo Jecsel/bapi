ffc14aa1c651c01a1aa66d81527575e11aa018d4
johnpatrickdelacruz <patrick.delacruz@biomarking.com>
2020-05-08 13:01:46 +0800

    revert changes

--- app/controllers/v1/guest/location_controller.rb
+++ app/controllers/v1/guest/location_controller.rb
@@ -8,31 +8,11 @@ class V1::Guest::LocationController < ApplicationController
     def until
         self.slot_time +10.minutes
     end
-
+    
     def find_schedules
         booking_date_range = Setting.last.booking_date_range
         @loc = Location.find params[:location_id]
         @schedules = @loc.schedules.where("id = ? && schedule_date > ?",params[:scheduled_id],cut_off_time).order(schedule_date: :asc).limit(booking_date_range)
-      
-        data = @schedules.any? ? @schedules.last.slots.group_by(&:meridian) : []
-        @data = data.map { |kev,val|
-            {
-                key: val.map { |slot|
-                    {
-                        id: slot.id,
-                        schedule_id: slot[:schedule_id],
-                        slot_time: slot.slot_time,
-                        meridian: slot.meridian,
-                        allocations: slot.allocations,
-                        created_at: slot.created_at,
-                        status: slot.status,
-                        updated_at: slot.updated_at,
-                        duration: slot.full_duration
-                    }
-                    
-                }
-            }
-        }
     end
     
     def schedules
