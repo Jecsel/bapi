b630080c035f06d1fc1d9d2e9b169aa8914574e4
Ramel <ramel.cabug@biomarking.com>
2020-06-03 08:34:32 +0800

    fix merge conflict

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -10,8 +10,6 @@ class BookingMailer < ApplicationMailer
     end
     def pay_later_email booking_id
         @booking = Booking.find booking_id
-        p "=========="
-        p @booking.clinic
         clinic_copy = @booking.clinic.present? && !@booking.clinic.email_address.nil?  ? @booking.clinic.email_address.split() : []
         mail(
             to: @booking.payment.user_email, 
@@ -19,8 +17,6 @@ class BookingMailer < ApplicationMailer
             bcc: clinic_copy,
             subject: "COVID-19 Drive-Through Booking Confirmation  | REF: #{@booking.reference_code}")
     end
-<<<<<<< HEAD
-=======
     
     def manual_confirmation booking_id
         @booking = Booking.find booking_id
@@ -31,5 +27,4 @@ class BookingMailer < ApplicationMailer
             bcc: clinic_copy,
             subject: "COVID-19 Drive-Through Booking Confirmation  | REF: #{@booking.reference_code}")
     end
->>>>>>> a0c022a04a08e8d4edbde89f3bfb90e3796037a3
 end
