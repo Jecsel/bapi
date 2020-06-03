b5d2b981cfea26bfcaf6433c8b142b5398d424b1
Ramel <ramel.cabug@biomarking.com>
2020-06-03 09:07:18 +0800

    build settings

--- app/controllers/v1/setting_controller.rb
+++ app/controllers/v1/setting_controller.rb
@@ -3,7 +3,15 @@ class V1::SettingController < ApplicationController
 
     def index
         @setting = Setting.last
-        render json: @setting
+        histories = @setting.setting_histories.where("setting_type = ?",1)
+        if histories.any?
+            render json: {
+                setting:@setting,
+                price_updated_by:histories.last.user.username, 
+                price_updated_date:histories.last.created_at }
+        else
+            render json: {setting:@setting}
+        end
     end
     def update
         @setting = Setting.last
@@ -12,11 +20,25 @@ class V1::SettingController < ApplicationController
         end
         case setting_params[:type]
             when 1
+                @setting.setting_histories.create(
+                    user_id:@current_user.id, 
+                    setting_type:setting_params[:type],
+                    old_value: @setting.covid_price,
+                    new_value: setting_params[:new_value]
+                )
                 @setting.update covid_price:setting_params[:new_value]
+                render json: {message:"Price updated successfully"}
             when 2
+                @setting.setting_histories.create(
+                    user_id:@current_user.id, 
+                    setting_type:setting_params[:type],
+                    old_value: @setting.booking_date_range,
+                    new_value:setting_params[:new_value]
+                )
                 @setting.update booking_date_range:setting_params[:new_value]
+                render json: {message:"Booking range updated"}
         end
-        render json: :saved
+        
     end
 
     private
