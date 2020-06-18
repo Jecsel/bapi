b5533bbcd939074593257a72ed8c37478e99d617
Ramel <ramel.cabug@biomarking.com>
2020-06-18 14:57:49 +0800

    fixed time

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -10,7 +10,7 @@ class Slot < ApplicationRecord
         where("is_deleted = ?",false)
     end
     def slot_time_with_interval
-        self.slot_time.utc.strftime("%I:%M")+ " - " + (self.slot_time + self.schedule.minute_interval*60).utc.strftime("%I:%M")
+        self.slot_time.utc.strftime("%I:%M %p")+ " - " + (self.slot_time + self.schedule.minute_interval*60).utc.strftime("%I:%M %p")
     end
 
     def self.count_total_available_slots 
