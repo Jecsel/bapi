0d1b47956a0c178283d9efb169c7cee060179daf
Ramel Cabug-os <ramel.cabug-os@biomarking.com>
2021-01-27 20:28:28 +0800

    revert sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 require 'sidekiq'
 require 'sidekiq/web'
 Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDIS_URL"]}
+    config.redis = { url: "redis://#{ENV['REDIS_URL']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB']}"}
 end
     
 Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDIS_URL"]}
+    config.redis = { url: "redis://#{ENV['REDIS_URL']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB']}"}
 end
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
     [user, password] == [ ENV['SIDEKIQ_USERNAME'], ENV['SIDEKIQ_PASSWORD']]
